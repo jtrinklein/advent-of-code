0022248b020559879783fcdf051ccb96fc552f17
James Trinklein <james.trinklein@changepoint.com>
2018-12-02 23:53:18 -0800

    chore: re-add part 1 for 2015/04

--- 2015/04-stocking.py
+++ 2015/04-stocking.py
@@ -9,8 +9,8 @@ def find_with_prefix(prefix, start=1, end=150000000):
     for i in range(start, end):
         h = hash(i)
         if h.startswith(prefix):
-            print('found it:', i)
+            print(f'first hash with prefix "{prefix}" is: {i} ({h})')
             return i
 
-# find_with_prefix('00000') -> 254575
+find_with_prefix('00000') #-> 254575
 find_with_prefix('000000', start=254575)
