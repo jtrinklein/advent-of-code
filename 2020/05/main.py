c8e0c5312468f405eb3e5be798afbc34cc254d2a
James Trinklein <1398935+jtrinklein@users.noreply.github.com>
2020-12-05 11:55:45 -0800

    2020 day 5: part 1

---
+++ 2020/05/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from os import getsid, replace
+
+data = []
+
+with open('./data.txt') as f:
+    data = [l.strip() for l in f.readlines()]
+    
+def get_value_from_bin_code(d, one_val):
+    v = 0
+    vb = [ 1 if x == one_val else 0 for x in d ]
+    vb.reverse()
+    for i, b in enumerate(vb):
+        v += b*(2**i)
+    return v
+
+def get_row(d):
+    return get_value_from_bin_code(list(d[:7]), 'B')
+
+def get_column(d):
+    return get_value_from_bin_code(list(d[7:]), 'R')
+
+def get_seat_id(d):
+    return get_row(d) * 8 + get_column(d)
+
+def part1(seats):
+    max_seat = max([get_seat_id(seat) for seat in seats])
+    print(max_seat)
+
+part1(data)
