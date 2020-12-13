3c2d933f02e14436003e7285415cb576c89c12c9
James Trinklein <1398935+jtrinklein@users.noreply.github.com>
2020-12-12 16:18:44 -0800

    2020 day 12: part 2

--- 2020/12/main.py
+++ 2020/12/main.py
@@ -6,14 +6,17 @@ class ShipInfo:
     '''
     class for keeping ship's information
     '''
-
+    wx: int
+    wy: int
     dir_index: int
     x: int
     y: int
-    def __init__(self, x, y, dir_index):
+    def __init__(self, x, y, dir_index, wx = 0, wy = 0):
         self.x = x
         self.y = y
         self.dir_index = dir_index
+        self.wx = wx
+        self.wy = wy
 
 data = []
 
@@ -47,16 +50,53 @@ def move_ship(instruction, magnitude, ship_info):
     ship_info.x += dx
     ship_info.y += dy
 
-def follow_course(instructions):
-    ship_info = ShipInfo(0, 0, directions.index('E'))
+def move_with_waypoint(instruction, magnitude, ship_info):
+    '''
+    Action N means to move the waypoint north by the given value.
+    Action S means to move the waypoint south by the given value.
+    Action E means to move the waypoint east by the given value.
+    Action W means to move the waypoint west by the given value.
+    Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+    Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+    Action F means to move forward to the waypoint a number of times equal to the given value.
+    '''
+    if instruction in 'LR':
+        for _ in range( magnitude // 90 ): # for however many rotations to perform
+            # swap wx,wy
+            ship_info.wx, ship_info.wy = ship_info.wy, ship_info.wx
+            
+            if instruction == 'L':
+                # invert wx for L rotation
+                ship_info.wx *= -1
+            else:
+                # invert wy for R rotation
+                ship_info.wy *= -1
+        return
+
+    if instruction == 'F':
+        ship_info.x += magnitude * ship_info.wx
+        ship_info.y += magnitude * ship_info.wy
+        return
+
+    # only NESW left
+    dx,dy = movement[instruction]
+    ship_info.wx += dx * magnitude
+    ship_info.wy += dy * magnitude
+
+
+def follow_course(instructions, move_function=move_ship):
+    ship_info = ShipInfo(0, 0, directions.index('E'), wx=10, wy=1)
     for instruction, magnitude in instructions:
-        move_ship(instruction, magnitude, ship_info)
-    
+        move_function(instruction, magnitude, ship_info)
     return abs(ship_info.x) + abs(ship_info.y)
 
 def part1(d):
     dist = follow_course(d)
     print(f'final distance: {dist}')
 
-part1(data)
+def part2(d):
+    dist = follow_course(d, move_function=move_with_waypoint)
+    print(f'final distance following waypoint: {dist}')
+
+part2(data)
 
