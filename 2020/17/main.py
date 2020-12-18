04478e5ee69143a68ecb7ef59784677da90f65b9
James Trinklein <1398935+jtrinklein@users.noreply.github.com>
2020-12-17 19:22:20 -0800

    2020 day 17: part 2

--- 2020/17/main.py
+++ 2020/17/main.py
@@ -5,43 +5,33 @@ data = []
 with open('./data.txt') as f:
     data = [[1 if x == '#' else 0 for x in list(line.strip())] for line in f.readlines()]
 
-'''
-Sample 1
-.#.
-..#
-###
-'''
-# data = [
-#     [0,1,0],
-#     [0,0,1],
-#     [1,1,1],
-# ]
-
-
 def key_to_point(k):
-    return [int(x) for x in k.split(',')]
-def point_to_key(x,y,z):
-    return f'{x},{y},{z}'
-def get_state_at(x,y,z, grid):
-    return grid.get(point_to_key(x,y,z), 0)
+    return k
 
-def set_state_at(x,y,z,value, grid):
-    grid[point_to_key(x,y,z)] = value
+def point_to_key(x,y,z,w):
+    return (x,y,z,w)
 
-def next_state(x,y,z,grid):
+def get_state_at(grid, x,y,z,w):
+    return grid.get(point_to_key(x,y,z,w), 0)
+
+def set_state_at(grid, value, x,y,z,w):
+    grid[point_to_key(x,y,z,w)] = value
+
+def next_state(grid,x,y,z,w,use_4_dimensions=False):
     '''
     If a cube is active 
-      and exactly 2 or 3 of its 26 neighbors are also active,
+      and exactly 2 or 3 of its neighbors are also active,
             the cube remains active.
       Otherwise, the cube becomes inactive.
     If a cube is inactive 
-        but exactly 3 of its 26 neighbors are active,
+        but exactly 3 of its neighbors are active,
             the cube becomes active. 
         Otherwise, the cube remains inactive.
     '''
-    total = sum(get_state_at(xi,yi,zi, grid) for zi in range(z-1, z+2) for yi in range(y-1, y+2) for xi in range(x-1, x+2))
+    wrange = range(w-1,w+2) if use_4_dimensions else [w]
+    total = sum(get_state_at(grid,xi,yi,zi,wi) for zi in range(z-1, z+2) for yi in range(y-1, y+2) for xi in range(x-1, x+2) for wi in wrange)
 
-    my_state = get_state_at(x,y,z,grid)
+    my_state = get_state_at(grid,x,y,z,w)
 
     # don't include my state when considering alive near me
     total -= my_state
@@ -55,47 +45,56 @@ def next_state(x,y,z,grid):
         return 1
     return 0
     
-def do_cycle(grid):
+def do_cycle(grid,use_4_dimensions=False):
     result = {}
     to_check = []
+
     for k,v in grid.items():
         # check active spaces
         if v == 1:
-            x,y,z = key_to_point(k)
-            p = [x,y,z]
-            if not p in to_check:
+            p = key_to_point(k)
+            x,y,z,w = p
+            if not p in result:
                 to_check.append(p)
-            # also check all 26 spaces touching this
-            for zi in range(z-1, z+2):
-                for yi in range(y-1, y+2):
-                    for xi in range(x-1, x+2):
-                        p = [xi,yi,zi]
-                        if not p in to_check:
-                            to_check.append(p)
-    for x,y,z in to_check:
-        state = next_state(x,y,z, grid)
-        set_state_at(x,y,z, state, result)
+            # also check all spaces touching this
+            # 26 if in 3 dimensions
+            # 80 if in 4 dimensions
+            wrange = range(w-1,w+2) if use_4_dimensions else [w]
+            for wi in wrange:
+                for zi in range(z-1, z+2):
+                    for yi in range(y-1, y+2):
+                        for xi in range(x-1, x+2):
+                            p = point_to_key(xi,yi,zi,wi)
+                            if not p in result:
+                                to_check.append(p)
+    for x,y,z,w in to_check:
+        state = next_state(grid, x,y,z,w, use_4_dimensions=use_4_dimensions)
+        set_state_at(result, state, x,y,z,w)
     return result
 
 def gridprint(grid):
     points = [key_to_point(k) for k,v in grid.items() if v == 1]
-    minz = min(z for x,y,z in points) if len(points) > 0 else 0
-    maxz = max(z for x,y,z in points) if len(points) > 0 else 0
-    miny = min(y for x,y,z in points) if len(points) > 0 else 0
-    maxy = max(y for x,y,z in points) if len(points) > 0 else 0
-    minx = min(x for x,y,z in points) if len(points) > 0 else 0
-    maxx = max(x for x,y,z in points) if len(points) > 0 else 0
+    minw = min(w for x,y,z,w in points)
+    maxw = max(w for x,y,z,w in points)
+    minz = min(z for x,y,z,w in points)
+    maxz = max(z for x,y,z,w in points)
+    miny = min(y for x,y,z,w in points)
+    maxy = max(y for x,y,z,w in points)
+    minx = min(x for x,y,z,w in points)
+    maxx = max(x for x,y,z,w in points)
 
-    for z in range(minz,maxz+1):
-        print(f'z: {z}')
-        for y in range(miny, maxy+1):
-            for x in range(minx, maxx+1):
-                if [x,y,z] in points:
-                    print('#', end='')
-                else:
-                    print('.', end='')
-            print('')
-        print('\n--------------')
+    for w in range(minw, maxw + 1):
+        for z in range(minz,maxz+1):
+            print(f'z: {z}, w: {w}')
+            for y in range(miny, maxy+1):
+                for x in range(minx, maxx+1):
+                    k = point_to_key(x,y,z,w)
+                    if k in points:
+                        print('#', end='')
+                    else:
+                        print('.', end='')
+                print('')
+            print('\n--------------')
     print('=================================')
 
 def build_grid(d):
@@ -103,17 +102,42 @@ def build_grid(d):
     for y in range(len(d)):
         for x in range(len(d[0])):
             if d[y][x] == 1:
-                set_state_at(x,y,0, 1, grid)
+                set_state_at(grid, 1, x,y,0,0)
     return grid
 
 def part1(d):
     grid = build_grid(d)
+    # gridprint(grid)
+    # grid = do_cycle(grid)
+    # gridprint(grid)
+    # grid = do_cycle(grid)
+    # gridprint(grid)
     for _ in range(6):
         grid = do_cycle(grid)
     c = sum(grid.values())
     print(f'{c} active cells')
 
+def part2(d):
+    grid = build_grid(d)
+    # gridprint(grid)
+    # grid = do_cycle(grid, use_4_dimensions=True)
+    # gridprint(grid)
+    for _ in range(6):
+        grid = do_cycle(grid, use_4_dimensions=True)
+    
+    c = sum(grid.values())
+    print(f'{c} active cells')
 
-part1(data)
+'''
+Sample 1
+.#.
+..#
+###
+'''
+# data = [
+#     [0,1,0],
+#     [0,0,1],
+#     [1,1,1],
+# ]
 
-
+part2(data)
